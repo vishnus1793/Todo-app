e01458aef4d93f52ebb4caed535c24536813175c
Vishnu S <153924683+vishnus1793@users.noreply.github.com>
2024-11-28 16:45:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [todos, setTodos] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [isEditing, setIsEditing] = useState(null);
+  const [editValue, setEditValue] = useState('');
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
@@ -21,7 +23,21 @@ function App() {
     setTodos(newTodos);
   };
 
-  // Handle pressing "Enter" to add a new todo
+  const handleEditClick = (index) => {
+    setIsEditing(index);
+    setEditValue(todos[index]);
+  };
+
+  const handleEditChange = (e) => {
+    setEditValue(e.target.value);
+  };
+
+  const handleSaveEdit = (index) => {
+    const newTodos = todos.map((todo, i) => (i === index ? editValue : todo));
+    setTodos(newTodos);
+    setIsEditing(null);
+  };
+
   const handleKeyPress = (e) => {
     if (e.key === 'Enter') {
       handleAddTodo();
@@ -31,18 +47,38 @@ function App() {
   return (
     <div className="App">
       <h1>To-Do List</h1>
-      <input 
-        type="text" 
-        value={inputValue} 
-        onChange={handleInputChange} 
-        onKeyPress={handleKeyPress}  // Add key press event listener
-        placeholder="Add a new task..." 
+      <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+        placeholder="Add a new task..."
       />
       <button onClick={handleAddTodo}>Add</button>
       <ul>
         {todos.map((todo, index) => (
           <li key={index}>
-            {todo} <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+            {isEditing === index ? (
+              <>
+                <input
+                  type="text"
+                  value={editValue}
+                  onChange={handleEditChange}
+                />
+                <button onClick={() => handleSaveEdit(index)}>Save</button>
+              </>
+            ) : (
+              <>
+                {todo}
+                <div className="dropdown">
+                  <button className="dropdown-button">Options ▼</button>
+                  <div className="dropdown-content">
+                    <button onClick={() => handleEditClick(index)}>Edit</button>
+                    <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+                  </div>
+                </div>
+              </>
+            )}
           </li>
         ))}
       </ul>
